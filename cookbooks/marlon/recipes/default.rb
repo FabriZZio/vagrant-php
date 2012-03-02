c7652479fcf7ed303f00b57f3d744f64f889e143
Dieter Provoost <contact@dieterprovoost.be>
2012-03-02 13:54:55 +0100

    oci8 driver integration with Oracle instantclient

--- cookbooks/marlon/recipes/default.rb
+++ cookbooks/marlon/recipes/default.rb
@@ -22,12 +22,12 @@ template "/etc/php5/conf.d/xdebug.ini" do
 end
 
 # apc requirement
-package "libpcre3-dev"
+#package "libpcre3-dev"
 
-php_pear "apc" do
-  action :install
-  notifies :restart, resources("service[apache2]"), :delayed
-end
+#php_pear "apc" do
+#  action :install
+#  notifies :restart, resources("service[apache2]"), :delayed
+#end
 
 # php5 mysql
 package "php5-mysql" do
@@ -41,17 +41,66 @@ execute "disable-default-site" do
   notifies :reload, resources(:service => "apache2"), :delayed
 end
 
-# configure apache vhost
+# configure apache project vhost
 web_app "project" do
   template "project.conf.erb"
-  notifies :reload, resources(:service => "apache2"), :delayed
+  server_name node['hostname']
+  server_aliases node['aliases']
+  docroot "/vagrant"
+  set_env node['set_env']
 end
 
 # install PHPUnit
 package "phpunit"
 
+# install unzip
+package "unzip"
+
+# oracle instantclient
+script "install_instantclient" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  rm -rf /tmp/instantclient*
+  wget --load-cookies=/vagrant/oracle-cookie.txt http://download.oracle.com/otn/linux/instantclient/11203/instantclient-basic-linux-11.2.0.3.0.zip
+  unzip instantclient-basic-linux-11.2.0.3.0.zip
+  
+  wget --load-cookies=/vagrant/oracle-cookie.txt http://download.oracle.com/otn/linux/instantclient/11203/instantclient-sqlplus-linux-11.2.0.3.0.zip
+  unzip instantclient-sqlplus-linux-11.2.0.3.0.zip
+  
+  wget --load-cookies=/vagrant/oracle-cookie.txt http://download.oracle.com/otn/linux/instantclient/11203/instantclient-sdk-linux-11.2.0.3.0.zip
+  unzip instantclient-sdk-linux-11.2.0.3.0.zip
+  
+  mkdir -p /usr/local/instantclient
+  rm -rf /usr/local/instantclient/*
+  cp -R instantclient_11_2/* /usr/local/instantclient/
+  cd /usr/local/instantclient
+  ln -s libclntsh.so.11.1 libclntsh.so
+  ln -s libocci.so.11.1 libocci.so
+  
+  echo 'export ORACLE_HOME=/usr/local/instantclient' >> /etc/profile
+  echo 'export DYLD_LIBRARY_PATH=/usr/local/instantclient' >> /etc/profile
+  
+  EOH
+  not_if "test -f /usr/local/instantclient"
+end
+
+# libaio requirement oci8
+package "libaio1"
+
+# php oci8
+script "oci8" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  echo 'instantclient,/usr/local/instantclient' | sudo pecl install oci8
+  echo '
+  extension=oci8.so' >> /etc/php5/conf.d/custom.ini
+  EOH
+end
 
 # todo:
-# vhost aliasses
 # apache /pma alias: "project.local/pma"
 # install php from dotdeb

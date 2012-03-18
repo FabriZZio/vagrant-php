6f6af51f2219e8f657d7975f250528476325559e
Dieter Provoost <contact@dieterprovoost.be>
2012-03-18 10:04:44 +0100

    apt sources update - use official ubuntu supported PHP version for now

--- cookbooks/marlon/recipes/apt-sources.rb
+++ cookbooks/marlon/recipes/apt-sources.rb
@@ -1,7 +1,9 @@
-apt_repository "launchpad" do
-  uri "http://ppa.launchpad.net/brianmercer/php5/ubuntu" # will not be supported after May 2012
-  components ["lucid","main"]
-  deb_src 1
-  keyserver "keyserver.ubuntu.com"
-  key "8D0DC64F"
-end
+# add sources for non-official repositories
+
+#apt_repository "launchpad" do
+#  uri "http://ppa.launchpad.net/brianmercer/php5/ubuntu" # will not be supported after May 2012
+#  components ["lucid","main"]
+#  deb_src 1
+#  keyserver "keyserver.ubuntu.com"
+#  key "8D0DC64F"
+#end

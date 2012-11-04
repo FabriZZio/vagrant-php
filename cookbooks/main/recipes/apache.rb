dc4d24fcab63ff38db3f6058ef2b4e44b12fdb80
Dieter Provoost <contact@dieterprovoost.be>
2012-11-04 11:52:44 +0100

    apache with php fastcgi

--- cookbooks/main/recipes/apache.rb
+++ cookbooks/main/recipes/apache.rb
@@ -1,22 +1,27 @@
 include_recipe "apache2"
 include_recipe "apache2::mod_headers"
 include_recipe "apache2::mod_deflate"
-include_recipe "apache2::mod_php5" # disable in favor of php-fpm
-#include_recipe "apache2::mod_fastcgi"
 
-#apache_module "actions" do
-#    enable true
-#end
+# fastcgi requirement
+package "libapache2-mod-fastcgi"
 
-#execute "change-fastcgi-owner-permissions" do
-#    command "sudo chown vagrant /var/lib/apache2/fastcgi"
-#    notifies :reload, resources(:service => "apache2"), :delayed
-#end
+apache_module "fastcgi" do
+    conf true
+end
 
-#execute "change-fastcgi-group-permissions" do
-#    command "sudo chgrp vagrant /var/lib/apache2/fastcgi"
-#    notifies :reload, resources(:service => "apache2"), :delayed
-#end
+apache_module "actions" do
+    enable true
+end
+
+execute "change-fastcgi-owner-permissions" do
+    command "sudo chown vagrant /var/lib/apache2/fastcgi"
+    notifies :reload, resources(:service => "apache2"), :delayed
+end
+
+execute "change-fastcgi-group-permissions" do
+    command "sudo chgrp vagrant /var/lib/apache2/fastcgi"
+    notifies :reload, resources(:service => "apache2"), :delayed
+end
 
 # disable default apache site
 execute "disable-default-site" do
@@ -24,13 +29,12 @@ execute "disable-default-site" do
   notifies :reload, resources(:service => "apache2"), :delayed
 end
 
-# default vhost containing FastCGIExternalServer declaration (can only be used once in all vhosts)
-#web_app "default" do
-#  server_name node['hostname']
-#  server_aliases []
-#  docroot "/vagrant/docs"
-#  template "web_app_fastcgi.conf.erb"
-#end
+# default vhost
+web_app "default" do
+  server_name node['hostname']
+  server_aliases []
+  docroot "/vagrant/docs"
+end
 
 # load vhost entries from Vagrantfile
 node['applications'].each do |a|

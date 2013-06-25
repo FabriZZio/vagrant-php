d93ca4bca6b94fd580805a1ea3d6d5d88cfc0d3e
Dieter Provoost <contact@dieterprovoost.be>
2013-06-25 10:03:54 +0200

    make sure mysql is started before trying to connect

--- cookbooks/main/recipes/mysql.rb
+++ cookbooks/main/recipes/mysql.rb
@@ -1,5 +1,12 @@
 # mysql
 include_recipe "openssl"
+
+# dirty hack to make sure mysql is running
+service "mysql" do
+  action :start
+  only_if "test -f /etc/init.d/mysql"
+end
+
 include_recipe "mysql::server"
 include_recipe "mysql_charset"
 
